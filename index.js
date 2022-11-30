70d67e59252406835a4d50e1db2b9d418e200d76
git-savio1.domi <techsavvydominic3@gmail.com>
2022-11-30 14:40:56 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(scuberGreetingForFeet){
+}
   // Write your code here!
+  function scuberGreetingForFeet(city) {
+
+    if (city <= 400){
+            return "This one is on me!";
+    }
+    else if (city > 2000 && city < 2500){
+        return "I will gladly take your thirty bucks.";
+    }
+    else if(city > 2500){
+        return "No can do.";
+    }
+  
+  }
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+}
+else{
+    return "No go.";
+}
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
