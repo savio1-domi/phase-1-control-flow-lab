44332a915e1aa78f5d22ab3fe24634f7fc197abd
git-savio1.domi <techsavvydominic3@gmail.com>
2022-12-14 11:47:01 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
-function scuberGreetingForFeet(scuberGreetingForFeet){
-}
+// let feet = 300
+function scuberGreetingForFeet(feet){
   // Write your code here!
-  function scuberGreetingForFeet(city) {
 
-    if (city <= 400){
-            return "This one is on me!";
-    }
-    else if (city > 2000 && city < 2500){
-        return "I will gladly take your thirty bucks.";
-    }
-    else if(city > 2500){
-        return "No can do.";
-    }
-  
+  if (feet < 400){
+    return 'This one is on me!' //This works
+  }else if(feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.';
   }
-  
-
+  else if(feet > 2500){
+    return 'No can do.'
+  }
+}
 function ternaryCheckCity(city){
-  // Write your code here!
-  if (city === "NYC") {
-    return "Ok, sounds good.";
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else if(city !== 'NYC'){
+    return 'No go.'
+  }
 }
-else{
-    return "No go.";
-}
-}
-
 function switchOnCharmFromTip(tip){
-  // Write your code here!
-  if (tip === "generous") {
-    return "Thank you so much.";
-} else if (tip === "not as generous") {
-    return "Thank you.";
-} else {
-    return "Bye.";
-}
+  if(tip === 'generous'){
+    return 'Thank you so much.'
+  }
+  else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  else{
+    return 'Bye.'
+  }
 }
