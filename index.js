bcde276281dc26ffaf0f83075838980072b751c1
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 14:58:17 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // let feet = 300
 function scuberGreetingForFeet(feet){
   // Write your code here!
-
+  // giving conditions
   if (feet < 400){
-    return 'This one is on me!' //This works
+    return 'This one is on me!' 
   }else if(feet > 2000 && feet <= 2500){
     return 'I will gladly take your thirty bucks.';
   }
@@ -12,6 +12,7 @@ function scuberGreetingForFeet(feet){
   }
 }
 function ternaryCheckCity(city){
+  //giving conditions
   if (city === 'NYC'){
     return 'Ok, sounds good.'
   }
@@ -20,6 +21,7 @@ function ternaryCheckCity(city){
   }
 }
 function switchOnCharmFromTip(tip){
+  //giving conditions
   if(tip === 'generous'){
     return 'Thank you so much.'
   }
